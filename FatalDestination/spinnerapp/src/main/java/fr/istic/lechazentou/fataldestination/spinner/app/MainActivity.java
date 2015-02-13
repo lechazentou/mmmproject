de8e90f6accb62524b4406c2a07a3ac573773410
JulesC <jules.chapron@gmail.com>
2015-02-13 12:45:12 +0100

    Implements fragment interface

--- FatalDestination/spinnerapp/src/main/java/fr/istic/lechazentou/fataldestination/spinner/app/MainActivity.java
+++ FatalDestination/spinnerapp/src/main/java/fr/istic/lechazentou/fataldestination/spinner/app/MainActivity.java
@@ -1,12 +1,15 @@
 package fr.istic.lechazentou.fataldestination.spinner.app;
 
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import fr.istic.lechazentou.fataldestination.spinner.map.MapFragment;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements MapFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +38,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //TODO Implement this method
     }
 }

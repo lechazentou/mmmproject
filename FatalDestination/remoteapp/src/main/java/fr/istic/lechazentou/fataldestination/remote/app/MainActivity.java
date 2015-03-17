8fbbc072ae5805bb5bea310f4a2587a192667876
Legw4nn <legeay.erwann@gmail.com>
2015-03-17 11:25:56 +0100

    Add: device list activity for bluetooth

--- FatalDestination/remoteapp/src/main/java/fr/istic/lechazentou/fataldestination/remote/app/MainActivity.java
+++ FatalDestination/remoteapp/src/main/java/fr/istic/lechazentou/fataldestination/remote/app/MainActivity.java
@@ -1,17 +1,53 @@
 package fr.istic.lechazentou.fataldestination.remote.app;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import fr.istic.lechazentou.fataldestination.connection.bluetooth.BluetoothService;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = "RemoteApp";
+
+    private static final int ACTION_SEND = 1;
+
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothService bluetoothService = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null){
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onStart(){
+        super.onStart();
+        Log.i(TAG, "Starting remote");
+        if (!bluetoothAdapter.isEnabled()){
+            Intent requestBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(requestBluetooth, REQUEST_ENABLE_BT);
+        } else {
+            if (bluetoothService == null){
+                bluetoothService = new BluetoothService(this, handler);
+            }
+        }
     }
 
 
@@ -35,5 +71,16 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private final Handler handler = new Handler();
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        switch (requestCode){
+            case REQUEST_CONNECT_DEVICE:
+                if (resultCode == Activity.RESULT_OK){
+                    String address = intent.getExtras()
+                }
+        }
     }
 }

3478ec8067fa0a1cc3189d6a3449cbeba83a16e8
Florian Etourneau <florian.etourneau@gmail.com>
2015-03-17 10:55:56 +0100

    Add : Accelero handling

--- FatalDestination/remoteapp/src/main/java/fr/istic/lechazentou/fataldestination/remote/app/MainActivity.java
+++ FatalDestination/remoteapp/src/main/java/fr/istic/lechazentou/fataldestination/remote/app/MainActivity.java
@@ -1,39 +1,115 @@
 package fr.istic.lechazentou.fataldestination.remote.app;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private boolean goodPos = false;
+    private TextView textView;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+
+
+
+    /** Called when the activity is first created. */
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote);
-    }
+        textView = (TextView) findViewById(R.id.textViewInfo);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lastUpdate = System.currentTimeMillis();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event);
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void getAccelerometer(SensorEvent sensorEvent) {
+
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+        long curTime = System.currentTimeMillis();
+
+        if ((curTime - lastUpdate) > 1500) {
+            long diffTime = (curTime - lastUpdate);
+            lastUpdate = curTime;
+
+            float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
+
+            String txt = " X : "  + x;
+
+            txt += "\n Y : "  + y;
+
+            txt += "\n Z : "  + z;
+
+            txt += "\n speed : " + speed;
+
+
+            if (-2 < x && x < 3 && 8 < y && -5 < z && z < 5){
+                textView.setBackgroundColor(Color.BLUE);
+                goodPos = true;
+            }
+            else if (x < -4 && 8 > y && -5 < z && z < 5 && goodPos){
+                textView.setBackgroundColor(Color.RED);
+            }
+            else if (-5 > z || z > 5 || x >2){
+                textView.setBackgroundColor(Color.WHITE);
+                goodPos = false;
+                sendSignal();
+            }
+
+            textView.setText(txt);
+
+            last_x = x;
+            last_y = y;
+            last_z = z;
+        }
+    }
+
+    private void sendSignal(){
+        // TODO
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // register this class as a listener for the orientation and
+        // accelerometer sensors
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // unregister listener
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
+

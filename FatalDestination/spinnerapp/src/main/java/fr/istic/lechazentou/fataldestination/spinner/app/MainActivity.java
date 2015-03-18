16ffa88ac69f9abb5735c7eae72ed6db42ef5eb9
JulesC <jules.chapron@gmail.com>
2015-03-18 16:28:02 +0100

    Bluetooth implemented on the app side

--- FatalDestination/spinnerapp/src/main/java/fr/istic/lechazentou/fataldestination/spinner/app/MainActivity.java
+++ FatalDestination/spinnerapp/src/main/java/fr/istic/lechazentou/fataldestination/spinner/app/MainActivity.java
@@ -208,13 +208,13 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.scan:
-                // Launch the DeviceListActivity to see devices and do scan
-                Intent serverIntent = new Intent(this, DeviceListActivity.class);
-                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
-                return true;
-            case R.id.discoverable:
+        if(item.getItemId() == R.id.scan){
+            // Launch the DeviceListActivity to see devices and do scan
+            Intent serverIntent = new Intent(this, DeviceListActivity.class);
+            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+            return true;
+        }
+        else if(item.getItemId() == R.id.discoverable) {
                 // Ensure this device is discoverable by others
                 ensureDiscoverable();
                 return true;

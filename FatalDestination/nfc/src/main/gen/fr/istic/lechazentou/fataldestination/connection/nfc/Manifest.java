45e19e385c071ebeb10328f8a84fccc2e3ab4382
JulesC <jules.chapron@gmail.com>
2015-03-20 09:27:23 +0100

    Clean entire project

--- FatalDestination/nfc/src/main/gen/fr/istic/lechazentou/fataldestination/connection/nfc/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package fr.istic.lechazentou.fataldestination.connection.nfc;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
